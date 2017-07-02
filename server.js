68551f2e42976b1fc9c05df479c8600c3f1d7881
Devashish2910 <dgpatel2910@gmail.com>
2017-07-01 23:59:49 -0500

    GET request added

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.post('/todos', (req, res) => {
   // add id of that data
   userData.id = createId();
   userData.description = userData.description.trim();
+  userData.createdDate = new Date().toString();
 
   // check if 'description' & 'status' is valid or send 400
   if(!_.isString(userData.description) || !_.isBoolean(userData.status) || userData.description.length === 0) {
@@ -49,11 +50,49 @@ app.post('/todos', (req, res) => {
 
 // GET request for showing data to user (R - Read Operation)
 app.get('/todos', (req, res) => {
+  const allTodos = todos;
+
   // check if has query parameter 'status'
+  if(req.query.hasOwnProperty('status') && (req.query.status === 'unfinished' || req.query.status === 'false')) {
+    // return unfinised/false todos
+    allTodos = _.where(allTodos, {status: false});
+  } else if (req.query.hasOwnProperty('status') && (req.query.status === 'finished' || req.query.status === 'true') {
+    // return finished/true todos
+    allTodos = _.where(allTodos, {status: true});
+  }
 
   // check if has query parameter 'q'
+  if(req.query.hasOwnProperty('q')) {
+    // return unfinised/false todos
+    allTodos = _.filter(allTodos, (cur) => {
+      return cur.description.toLowerCase().indexOf(req.query.q.trim().toLowerCase()) > -1;
+    });
+  }
 
   // send data back
+  if(allTodos.length > 0) {
+    res.json(allTodos);
+  } else {
+    res.status(404).send("No data found.");
+  }
+
+});
+
+// GET request for showing data by id
+app.get('/todos/:id', (req, res) => {
+  // convert parameter to int
+  const id = parseInt(req.params.id);
+
+  // check if available or send 404
+  const data = _.findWhere(todos, {id: id});
+
+  //send back to user
+  if(data !== undefined) {
+    res.json(data);
+  } else {
+    res.status(404).send("No data found.");
+  }
+
 });
 
 // initialize port for app
